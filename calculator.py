0942d7f8a95fa41cf48d638ddd4fbe8c1cd8eab8
shashankduttmr <shashankduttm@gmail.com>
2023-01-07 11:39:03 +0530

    first commit

--- calculator.py
+++ calculator.py
@@ -5,20 +5,38 @@ def Calculator():
 
     try:
         inp = int(input('enter your Choice:\n'))
-        if(inp > len(operations)):
+        if (inp > len(operations)):
             print('invalid input Enter valid value')
         else:
             def Add(n1, n2):
                 return n1+n2
+
             def Subtract(n1, n2):
                 return n1-n2
+
             def Multiple(n1, n2):
                 return n1*n2
+
             def Division(n1, n2):
                 return n1/n2
             print(f'Your Choice is {operations[inp]}')
+            n1 = float(input('Enter first value:\n'))
+            n2 = float(input('Enter second value:\n'))
+            if (operations[inp] == '-'):
+                result = Subtract(n1, n2)
+                print(f'{n1} - {n2} = {result}')
+            elif(operations[inp] == '+'):
+                result = Add(n1, n2)
+                print(f'{n1} + {n2} = {result}')
+            elif(operations[inp] == '*'):
+                result = Multiple(n1, n2)
+                print(f'{n1} * {n2} = {result}')
+            elif(operations[inp] == '/'):
+                result = Division(n1, n2)
+                print(f'{n1} / {n2} = {result}')
+
     except:
-        print('Please enter valid Input')   
+        print('Please enter valid Input')
 
 
 Calculator()
